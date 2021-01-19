46b1f816c551ccd11b1548bae503721ebb7b1be4
ClaFiore <claborghini@gmail.com>
2021-01-19 17:04:49 -0500

    added comments

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 new Vue({
     el: '#app',  //string that identifies the template in html - it's a DOM query selector
     methods: {
-        onInput: function(){    //onInput is a made up name, you can call this method whatever you want
-            console.log('typing smtg')
+        onInput: function(event){    //onInput is a made up name, you can call this method whatever you want
+            console.log(event)
+            
         }
     },
 })
