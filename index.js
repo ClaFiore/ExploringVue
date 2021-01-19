bc5d3d62747bf82104a99867904ea169a1c39701
ClaFiore <claborghini@gmail.com>
2021-01-19 17:05:28 -0500

    examining event.target in vue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ new Vue({
     el: '#app',  //string that identifies the template in html - it's a DOM query selector
     methods: {
         onInput: function(event){    //onInput is a made up name, you can call this method whatever you want
-            console.log(event)
+            console.log(event.target.value)
             
         }
     },
