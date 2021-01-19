26c514ff777ac0978958d81358cdccee7451defa
ClaFiore <claborghini@gmail.com>
2021-01-19 16:15:28 -0500

    defined first onInput method

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 new Vue({
-    el: '#app'  //string that identifies the template in html - it's a DOM query selector
-    
+    el: '#app',  //string that identifies the template in html - it's a DOM query selector
+    methods: {
+        onInput: function(){    //onInput is a made up name, you can call this method whatever you want
+            console.log('typing smtg')
+        }
+    },
 })
