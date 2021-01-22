7135fafdea116a6d8ed636b030e1fc767a25091b
ClaFiore <claborghini@gmail.com>
2021-01-22 15:27:50 -0500

    event handler

--- video-project/src/components/SearchBar.vue
+++ video-project/src/components/SearchBar.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        <input />
+       <!-- <input v-on:input="onInput"/> This is one way to write this event handler, you can write it also like the below -->
+       <input @input="onInput"/>
     </div>
 </template>
 
 
 <script>
     export default {
-        name: "SearchBar"
+        name: "SearchBar",
+        methods: {
+            //methods give the ability to change 'data'
+            onInput: function(event){
+                console.log(event.target.value)
+            } ,
+        }
     }
 </script>
 
