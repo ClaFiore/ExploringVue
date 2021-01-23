bd57677cf3e2e7103a0a0ad21e2ca2a5e4e856b9
ClaFiore <claborghini@gmail.com>
2021-01-23 15:06:16 -0500

    added VideoListItem component

--- video-project/src/components/VideoList.vue
+++ video-project/src/components/VideoList.vue
@@ -1,14 +1,26 @@
 <template>
     <div>
         <ul>
-            VideoList
+            <VideoListItem />
         </ul>
     </div>
 </template>
 
+
 <script>
+
+import VideoListItem from './VideoListItem'
+
 export default {
-    name: "VideoList"
+    name: "VideoList",
+    props: ['videos',], //this is an array with the names of all props being received 
+    components: {
+        VideoListItem,
+    }
+    // alternatively you can (optionally) speciffy what type of data your props is like the below
+    // props: {
+    //     videos: Array
+    // }
 }
 </script>
 
